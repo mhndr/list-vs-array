fbb0b266ae1d8e1e507d6166cf42a9d4a22751a0
Mahendar Srinivasan <mahendarin@gmail.com>
2024-05-23 00:34:45 +0530

    Update profile_list.d

--- profile_list.d
+++ profile_list.d
@@ -1,4 +1,3 @@
-
 dtrace:::BEGIN
 {
     printf("Starting to measure List Insert function runtime...\n");
@@ -10,13 +9,17 @@ dtrace:::BEGIN
 ListInsert$target:::entry
 {
     /*print(arg0);*/
+    self->arg = arg0;
     self->start = timestamp;
 }
 
 ListInsert$target:::exit
 /self->start != 0/
 {
-    @avgt= avg(timestamp - self->start);
+    self->diff = timestamp - self->start;
+    printf(" - took %dns to insert ",self->diff); 
+    print(self->arg);
+    @avgt= avg(self->diff);
     @count = count();
     self->start = 0;
 }
@@ -27,5 +30,4 @@ dtrace:::END
 /*    normalize(@avgt, 1000); */
     printa("Average time per execution (ns):  %-@8d\n", @avgt);
 }
-
 
