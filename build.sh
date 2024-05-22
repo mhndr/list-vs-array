54a0f78095cb5a73d0564b547611f78b4b6a25bb
Mahendar Srinivasan <mahendarin@gmail.com>
2024-05-22 14:53:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 
 cc  -g ./sl_list.c -o sl_list
-/usr/libexec/PlistBuddy -c "Add :com.apple.security.get-task-allow bool true" tmp.entitlements
-codesign -s - -f --entitlements tmp.entitlements  sl_list
+cc  -g ./array.c -o array
